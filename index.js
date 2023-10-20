fc3370f731572ac2393da291cde53117e12f86a7
shahchhatru <shahkhesavchhatra@gmail.com>
2023-10-20 12:28:44 +0545

    redux thunk practice

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
-import { createStore ,applyMiddleware} from 'redux'
+import { createStore ,applyMiddleware, combineReducers} from 'redux'
 import logger from 'redux-logger';
-
+import axios from 'axios';
 //store
-const store = createStore(reducer,applyMiddleware(logger.default));
+const init="init";
+const inc="increment";
+const dec="decrement";
+const incByAmt="incrementByAmount";
+const incBonus="increaseBonus";
+
+const store = createStore(combineReducers({
+    account:accountReducer,
+    bonus:bonusReducer
+}),applyMiddleware(logger.default));
 
 let logs=[];
 //reducer
-function reducer(state={amount:1},action){
-    if(action.type==='increment'){
-        return {amount:state.amount+1}
+function accountReducer(state={amount:1},action){
+
+    switch(action.type){
+        case init:
+            return{amount:action.payload}
+        case inc:
+            return {amount:state.amount+1}
+        case dec:
+            return {amount:state.amount-1}
+        case incByAmt:
+            return{amount:state.amount+action.payload}
+        default:
+            return state;
     }
     return state;
+}
+
+function bonusReducer(state={points:0},action){
+    switch(action.type){
+        case incBonus:
+            return {points:state.points+1}
+        case incByAmt:
+            if( action.payload>=200){
+                return {points:state.points+1}
+            }
+            return state;
+        default:
+            return state;
+    }
+}
+//action creators
+function initUser(value){
+    return {
+        type:init,
+        payload:value,
+    }
+}
+
+
+function incrementBonus(){
+    return {type:incBonus};
 }
 
 //global state
@@ -22,8 +67,10 @@ store.subscribe(()=>{
 
 
 setInterval(()=>{
-    store.dispatch({type:"increment"})
+    // store.dispatch({type:"increment"})
+    store.dispatch(initUser(500));
 },2000);
+
 
 
 // console.log(store.getState())
